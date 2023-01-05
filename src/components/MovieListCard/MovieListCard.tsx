a60f14c061bc20481ca09cc4666bf9208efa0458
Emanuel Zapata <emanuelzapata@Emanuels-MacBook-Pro.local>
2023-01-04 23:50:52 -0600

    committing to update code

--- src/components/MovieListCard/MovieListCard.tsx
+++ src/components/MovieListCard/MovieListCard.tsx
@@ -3,8 +3,11 @@ import './MovieListCard.css';
 
 export default function MovieListCard(props:any){
     console.log(props);
+    let test = () => {
+       console.log("working"); 
+    }
     return(
-        <div className="movie-list-card-container">
+        <div className="movie-list-card-container" onClick={test}>
             <img className="movie-poster" src={props.movie.Poster} />
             <p> {props.movie.personalNote} </p>
             <p> {props.movie.imdbRating} </p>
