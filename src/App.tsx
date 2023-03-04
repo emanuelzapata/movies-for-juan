89e57624a3502579afc97e5dcf100fc1cb26ab89
Emanuel Zapata <emanuelzapata@Emanuels-MBP.attlocal.net>
2023-03-04 02:20:17 -0600

    committing to push rewrite

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import axios, { AxiosResponse } from 'axios';
 import movies from './assets/movies.json';
-import MovieListCard from './components/MovieListCard/MovieListCard';
-import MovieModal from './components/MovieModal/MovieModal';
-
 export default function App() {
-  let modalMovie: any;
-
-  function practice(movie:any){
-    modalMovie = movie;
-    // console.log(movie);
-    //console.log(modalMovie);
-  }
 
   return (
     <div className="container">
-      {movies.map(movie =>(
-        <div onClick={()=>practice(movie)}>
-          <MovieListCard movie={movie} />
-        </div>
-      ))}
-      <MovieModal movie={modalMovie} />
+      <ul>
+      { movies.map((movie) => 
+        <li>{movie.title}</li>
+      )}
+      </ul>
     </div>
   );
 }
