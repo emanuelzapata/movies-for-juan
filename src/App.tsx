d0867b1c7542ec57e4d08f840807d7fb457bb0f6
Emanuel Zapata <emanuelzapata@Emanuels-MBP.attlocal.net>
2023-05-29 02:10:04 -0500

    committing to push working axios request to api

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import movies from './assets/movies.json';
+// import movies from './assets/movies.json';
 import { Icon } from './components/Icon/Icon';
 import Grid from '@mui/material/Grid'; // Grid version 1
+import axios from 'axios';
+
+// function getMovieData(){
+//   const response = axios.post("https://movies-for-juan.web.app/getMovies");
+//   return response;
+//   // return axios.post("https://movies-for-juan.web.app/getMovies").then((response)=>{
+//   //   return response.data;
+//   // });
+// }
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  useEffect(()=>{
+    axios.post("https://movies-for-juan.web.app/getMovies").then(async (response)=>{
+      setMovies(await response.data);
+    }).catch((error)=>{
+      console.log(error.response);
+    })
+  },[])
   return (
     <React.Fragment>
       <Grid justifyContent="center" alignItems="center" container>
